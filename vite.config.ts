9094988a46c522028f5039f70cb9750c50b4583c
yahokatk <yahokatk@gmail.com>
2026-05-11 16:25:29 +0300

    add  base: '/Portfolio2.0/' in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 import babel from '@rolldown/plugin-babel';
 
 export default defineConfig({
+  base: '/Portfolio2.0/',
   plugins: [
     react(),
     babel({
